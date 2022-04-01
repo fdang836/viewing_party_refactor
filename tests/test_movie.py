4afdf2b0532252f6256eabd2f9ec97bc9f2aff2e
Fena Dang <fenadang@gmail.com>
2022-04-01 10:27:32 -0700

    Finished class Movie and Person w/ test

--- tests/test_movie.py
+++ tests/test_movie.py
@@ -1,9 +1,22 @@
+from lib2to3.pgen2.pgen import generate_grammar
 import pytest
+from viewing_party.movie import *
 
 def test_1():
     # Arrange
+    title = "Travelers"
+    genre = "Drama"
+    rating = 9
+    host = "Netflix"
+    new_rating = 8
 
     # Act
+    movie = Movie(title, genre, rating, host)
+    movie.update_rating(new_rating)
 
     # Assert
-    pass
+    assert movie.title == "Travelers"
+    assert new_rating == movie.rating
+
+
+    
--- tests/test_person.py
+++ tests/test_person.py
@@ -1,9 +1,20 @@
 import pytest
+from viewing_party.person import *
 
 def test_1():
     # Arrange
+    name = "Olena"
+    watched = ["A", "B", "C"]
+    friends = ["D", "E", "F"]
+    subscriptions = ["Netflix", "Hulu"]
+    new_movie = "G"
 
     # Act
+    person = Person(name, watched, friends, subscriptions)
+    watched = person.add_watched(new_movie)
+    num_watched = person.get_num_watched()
 
     # Assert
-    pass
+    assert person.name == "Olena"
+    assert person.watched == ["A", "B", "C", "G"]
+    assert len(person.watched) ==  4
